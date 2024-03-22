d0b10e2c3400dea522dc87c2a7d428e9a22d761c
Lokesh-30 <lokeshrcer@gmail.com>
2024-03-22 06:22:19 +0530

    API and Paging Source Integration

---
+++ app/src/main/java/com/example/mynewsapp/di/Module.kt
@@ -0,0 +1,26 @@
+package com.example.mynewsapp.di
+
+import com.example.mynewsapp.NewsApp
+import com.example.mynewsapp.retrofit.ApiServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Class used as a injection module or dagger
+ */
+@InstallIn(SingletonComponent::class)
+@Module
+class Module {
+
+    /**
+     * News api service initializing
+     */
+    @Singleton
+    @Provides
+    fun provideApiServices(): ApiServices {
+        return  NewsApp().getRestClient().getApiService()
+    }
+}
---
+++ app/src/main/java/com/example/mynewsapp/paging/NewsPagingSource.kt
@@ -0,0 +1,33 @@
+package com.example.mynewsapp.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.mynewsapp.models.Article
+import com.example.mynewsapp.retrofit.ApiServices
+import java.lang.Exception
+
+class NewsPagingSource(private val apiServices: ApiServices) : PagingSource<Int, Article>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        return try {
+            val pageNumber = params.key ?: 0
+            val response = apiServices.getTopHeadings()
+
+            return LoadResult.Page(
+                data = response.body()?.articles ?: emptyList(),
+                prevKey = if (pageNumber > 0) pageNumber - 1 else null,
+                nextKey = if ((response.body()?.articles?.size ?: 0) < 10) null else pageNumber + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+}
